67f03d0f3f824370ae24dea4706dd2635097486e
Wojtek <wojtekj1989@gmail.com>
2019-03-04 20:35:41 +0100

    Created url handlers

---
+++ src/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from rest_framework.routers import SimpleRouter
+
+from .views import MoviesView, CommentsView, TopView
+
+router = SimpleRouter()
+router.register(r'movies', MoviesView)
+router.register(r'comments', CommentsView)
+router.register(r'top', TopView)
+
+urlpatterns = router.urls
