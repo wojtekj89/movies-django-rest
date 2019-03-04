43cfc9b90a26df012a66be15c6705c28b2d25646
Wojtek <wojtekj1989@gmail.com>
2019-03-04 23:26:55 +0100

    Added more test cases

--- src/api/tests/test_views.py
+++ src/api/tests/test_views.py
@@ -32,18 +32,48 @@ class MoviesCreateTest(TestCase):
         response = MoviesView.as_view({"post": "create"})(request)
         self.assertEqual(response.status_code, 201)
         payload = response.data
-        self.assertEqual(len(payload), 2)
         self.assertTrue(self.title in str(payload).lower())
     
     def test_add_existing_movie(self):
-        request = self.factory.post('/movies', data={'title': self.title})
+        request = self.factory.post('/movies', data={'title': "Existing"})
         response = MoviesView.as_view({"post": "create"})(request)
         self.assertEqual(response.status_code, 409)
+        payload = response.data
         self.assertEqual(len(payload), 1)
-
 
     def test_add_without_title(self):
         request = self.factory.post('/movies', data={'asd': self.title})
         response = MoviesView.as_view({"post": "create"})(request)
         self.assertEqual(response.status_code, 400)
+
+    def test_create_existing_short_title(self):
+        movie = mixer.blend('api.Movie', title="Game of Thrones")
+        request = self.factory.post('/movies', data={'title': "Game"})
+        response = MoviesView.as_view({"post": "create"})(request)
+        self.assertEqual(response.status_code, 409)
+
+    
+class MovieCreateAndGetTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.title = "Test"
+
+    def test_add_and_read(self):
+        request = self.factory.get('/movies')
+        response = MoviesView.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        payload = response.data
+        self.assertEqual(len(payload), 0)
+
+        request = self.factory.post('/movies', data={'title': self.title})
+        response = MoviesView.as_view({"post": "create"})(request)
+        self.assertEqual(response.status_code, 201)
+
+        request = self.factory.get('/movies')
+        response = MoviesView.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        payload = response.data
         self.assertEqual(len(payload), 1)
+        self.assertTrue(self.title in str(payload))
+
