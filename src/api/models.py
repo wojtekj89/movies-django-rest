b78e6f2690fd744574c68ce5d1de17582d4d7f2a
Wojtek <wojtekj1989@gmail.com>
2019-03-04 19:17:24 +0100

    Implemented models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db.models import Model, CharField, ForeignKey, TextField, DateTimeField, CASCADE
 
-# Create your models here.
+class Movie(Model):
+    title = CharField(max_length=100)
+    year = CharField(max_length=5)
+    genre = CharField(max_length=50)
+    country = CharField(max_length=50)
+    plot = TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(Model):
+    movie_id = ForeignKey(Movie, on_delete=CASCADE)
+    text = TextField()
+    timestamp = DateTimeField(auto_now_add=True)
