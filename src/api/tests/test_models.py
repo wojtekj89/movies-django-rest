de91854f360795b7d9e82c579a640d52d3565d2e
Wojtek <wojtekj1989@gmail.com>
2019-03-04 19:55:54 +0100

    Changed tests for models

--- src/api/tests/test_models.py
+++ src/api/tests/test_models.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
-import pytest
 
 from mixer.backend.django import mixer
-pytestmark = pytest.mark.django_db
 
 class TestModels(TestCase):
 
     def test_movie_model(self):
         movie = mixer.blend('api.Movie')
-        self.assertEqual(movie.pk, 1)
+        self.assertGreaterEqual(movie.pk, 1)
 
     def test_comment_model(self):
         comment = mixer.blend('api.Comment')
-        self.assertEqual(comment.pk, 1)
+        self.assertGreaterEqual(comment.pk, 1)
 
     def test_movie_string_representation(self):
         movie = mixer.blend('api.Movie')
