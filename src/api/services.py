ce1c6877b5e7709aa2c9acbd4c6eaab053e42e97
Wojtek <wojtekj1989@gmail.com>
2019-03-04 18:23:22 +0100

    Basic service implementation

--- src/api/services.py
+++ src/api/services.py
@@ -1,4 +1,14 @@
 import requests
+from core import settings
 
 def get_movie(title):
-    return None
+    """Getting movie details from OMDB API"""
+    params = {
+        't': title,
+        'apikey': settings.OMDB_API_KEY
+    }
+
+    r = requests.get(settings.OMDB_URL, params=params)
+    response = r.json()
+
+    return response
