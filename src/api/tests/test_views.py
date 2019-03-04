d6de932a14d4cbafd3b94a6428b0f22d85568072
Wojtek <wojtekj1989@gmail.com>
2019-03-04 22:42:36 +0100

    Created tests for adding new movie to the database

--- src/api/tests/test_views.py
+++ src/api/tests/test_views.py
@@ -4,7 +4,7 @@ from mixer.backend.django import mixer
 from api.views import MoviesView
 
 
-class MoviesViewTest(TestCase):
+class MoviesListTest(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
@@ -19,3 +19,31 @@ class MoviesViewTest(TestCase):
         payload = response.data
         self.assertEqual(len(payload), 2)
         self.assertTrue(str(self.movie1) in str(payload))
+
+class MoviesCreateTest(TestCase):
+    
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.movie2 = mixer.blend('api.Movie', title="Existing")
+        self.title = 'mock'
+    
+    def test_add_movie(self):
+        request = self.factory.post('/movies', data={'title': self.title})
+        response = MoviesView.as_view({"post": "create"})(request)
+        self.assertEqual(response.status_code, 201)
+        payload = response.data
+        self.assertEqual(len(payload), 2)
+        self.assertTrue(self.title in str(payload).lower())
+    
+    def test_add_existing_movie(self):
+        request = self.factory.post('/movies', data={'title': self.title})
+        response = MoviesView.as_view({"post": "create"})(request)
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(len(payload), 1)
+
+
+    def test_add_without_title(self):
+        request = self.factory.post('/movies', data={'asd': self.title})
+        response = MoviesView.as_view({"post": "create"})(request)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(len(payload), 1)
