a39e20a26f7b8c8cb7047d33637310935e6401f6
Wojtek <wojtekj1989@gmail.com>
2019-03-04 23:22:52 +0100

    Added POST method to movies view

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -6,3 +6,13 @@ class MovieSerializer(ModelSerializer):
     class Meta:
         model = Movie
         fields = ('id', 'title', 'year', 'genre', 'country', 'plot')
+
+class MovieRequestSerializer(Serializer):
+    """Checking if POST request is valid """
+    title = CharField(max_length=100, required=True)
+
+    def create(self, data):
+        return {'title': data.get('title')}
+
+    def update(self, instance, data):
+        instance['title'] = data.get('title', instance['title'])
--- src/api/views.py
+++ src/api/views.py
@@ -1,13 +1,37 @@
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+from rest_framework.response import Response
 
 from .models import Movie
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, MovieRequestSerializer
+
+from .services import get_movie
 
 class MoviesView(ListModelMixin, GenericViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
     def create(self, request, *args, **kwargs):
-        pass
+        request = MovieRequestSerializer(data=request.data)
+
+        if not request.is_valid():
+            return Response(request.errors, status=HTTP_400_BAD_REQUEST)
+
+        title = request.data['title']
+
+        if Movie.objects.filter(title=title).exists():
+            return Response({'error': 'Movie already exists'}, status=HTTP_409_CONFLICT)
+
+        omdb_data = get_movie(title)
+        movie = Movie(
+            title=omdb_data['Title'], 
+            year=omdb_data['Year'], 
+            genre=omdb_data['Genre'],
+            country=omdb_data['Country'],
+            plot=omdb_data['Plot']
+            )
+
+        movie.save()
+
+        return Response(MovieSerializer(movie).data, status=HTTP_201_CREATED)
