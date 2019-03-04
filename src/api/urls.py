9add0455fa87161198ded3cab180f932f97f52bd
Wojtek <wojtekj1989@gmail.com>
2019-03-04 23:58:30 +0100

    Commented unused code in urls to avoid compilation error

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 
 from rest_framework.routers import SimpleRouter
 
-from .views import MoviesView, CommentsView, TopView
+from .views import MoviesView#, CommentsView, TopView
 
 router = SimpleRouter()
 router.register(r'movies', MoviesView)
-router.register(r'comments', CommentsView)
-router.register(r'top', TopView)
+# router.register(r'comments', CommentsView)
+# router.register(r'top', TopView)
 
 urlpatterns = router.urls
