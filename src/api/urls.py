55dddb42164712d5319f3a03c19b97790b858cb6
Wojtek <wojtekj1989@gmail.com>
2019-03-05 21:25:27 +0100

    Updated urls with new views

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from rest_framework.routers import SimpleRouter
 
-from .views import MoviesView#, CommentsView, TopView
+from .views import MoviesView, CommentsView, TopView
 
 router = SimpleRouter()
 router.register(r'movies', MoviesView)
-# router.register(r'comments', CommentsView)
-# router.register(r'top', TopView)
+router.register(r'comments', CommentsView, base_name='comments')
+urlpatterns = [
+    url(r'top', TopView.as_view())
+]
 
-urlpatterns = router.urls
+urlpatterns += router.urls
--- src/core/urls.py
+++ src/core/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url('^', include('api.urls'))
 ]
