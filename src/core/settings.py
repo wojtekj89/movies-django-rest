a9bf53bcd8b779cab9c614a9767979ec68477ac9
Wojtek <wojtekj1989@gmail.com>
2019-03-05 21:32:30 +0100

    Removig debug mode for production

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v+^%ulee3-gwoem35a7#($shnot7iv7c5rd6k&(x5@^$y$@xkx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['wjmovies.herokuapp.com']
 
